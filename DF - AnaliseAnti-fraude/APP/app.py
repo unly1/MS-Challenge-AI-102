cebd5020239ce5bbe42b93cbc7bb8c0c4a3aceb7
Mario Alexandre <92002986+unly1@users.noreply.github.com>
2024-11-22 16:00:00 -0400

    Add files via upload

---
+++ DF - AnaliseAnti-fraude/APP/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from services.blob_service import upload_blob
+from services.credit_card_service import analyze_credit_card
+
+def configure_interface():
+    st.title("Upload de Arquivos DIO - Desafio 2 - Azure - Fake Docs")
+    uploaded_file = st.file_uploader("Escolha um arquivo", type=["png", "jpg", "jpeg"])
+
+    if uploaded_file is not None:
+        fileName = uploaded_file.name
+        # Enviar para o blob storage
+        blob_url = upload_blob(uploaded_file.getvalue(), fileName)
+        if blob_url:
+            st.write(f"Arquivo {fileName} enviado com sucesso para o Azure Blob Storage")
+            credit_card_info = analyze_credit_card(blob_url) # Chamar a função de detecção de informações de Cartão de Crédito
+            show_image_and_validation = (blob_url, credit_card_info)
+        else:
+            st.write(f"Erro ao enviar o arquivo {fileName} para o Azure Blob Storage")
+
+def show_image_and_validation(blob_url, credit_card_info):
+    st.image(blob_url, caption="Imagem envida", use_column_width=True)
+    st.write("Resultado da validação:")
+    if credit_card_info and credit_card_info ["card_name"]:
+        st.markdown(f"<h1 style='color: green;'>Cartão Válido</h1>", unsafe_allow_html=True)
+        st.write(f"Nome do Titular: {credit_card_info['card_name']}")
+        st.write(f"Banco Emissor: {credit_card_info['bank_name']}")
+        st.write(f"Data de Validade: {credit_card_info['expiry_date']}")
+    else:
+        st.markdown(f"<h1 style='color: red;'>Cartão Inválido</h1>", unsafe_allow_html=True)
+        st.write("Este não é um cartão de crédito válido")
+
+if __name__ == "__main__":
+    configure_interface()
